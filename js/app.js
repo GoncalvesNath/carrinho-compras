e7590d89946039793952921262b6e15e107b48e5
Nathalia Dias <158405216+ndiasg@users.noreply.github.com>
2024-11-07 14:31:16 -0300

    adicionando itens ao carrinho com subtotal atualizado

--- js/app.js
+++ js/app.js
@@ -3,15 +3,16 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1]; 
-    let quantidade = document.getElementById('quantidade'); 
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    //calcular o preço subtotal
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
-    
+    let quantidade = document.getElementById('quantidade').value; 
+    //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
     //adicionar itens ao carrinho
+        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+    
+    
     //atualizar valor total
 }
 
