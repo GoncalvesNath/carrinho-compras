8a1d390e7f6d6ec791e4e512a5f880d9ce0b7fae
Nathalia Dias <158405216+ndiasg@users.noreply.github.com>
2024-11-07 14:46:30 -0300

    atualizando carrinho e implementando função limpar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let valorTotal = 0;
+limpar();
 function adicionar() {
     //recuperar nomee o prroduto e quantidaade
     let produto = document.getElementById('produto').value;
@@ -11,11 +13,17 @@ function adicionar() {
         carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
   </section>`;
-    
-    
     //atualizar valor total
+    valorTotal = valorTotal + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${valorTotal}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar() {
+    valorTotal = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+
 
 }
